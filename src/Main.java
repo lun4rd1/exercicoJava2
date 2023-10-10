f3af591ce56a76ad98c4c6f9505afe0b39cee986
FIC <fic@info.local>
2023-10-09 21:46:46 -0300

    Certa

--- src/Main.java
+++ src/Main.java
@@ -177,7 +177,7 @@ public class Main {
                                         System.out.println("Endereço: " + cadaPj.endereco.logradouro + "-" + cadaPj.endereco.numero);
 
                                         System.out.println();
-                                        System.out.println("Aperte ENTER para continuar a a a a");
+                                        System.out.println("Aperte ENTER para continuar");
                                         leitor.nextLine();
 
                                     }
